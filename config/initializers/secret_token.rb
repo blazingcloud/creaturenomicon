1d3e29ba7ef1bee07eeae55708482f87209193b7
lori <lori.hsu@gmail.com>
2013-02-01 10:56:56 -0800

    new rails app with rspec initialized, and basic gems for UI and cloudinary

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creaturenomicon::Application.config.secret_token = 'f6ac1f4ba2a526cdf66fcbe490c49271baf1a21fc89f835a78c5912d6d5c0e313d3652ccc5d36e8f430ec3ded9dda1f560109c29a86ad59005f998c164751953'
